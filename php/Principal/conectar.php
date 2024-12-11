64468342c72214bd3e353345c6a9aa8519c2a98d
Marvin Tavares Lopes <escorpiano18@bol.com.br>
2024-12-11 19:06:46 -0300

    ok

--- php/Principal/conectar.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli('localhost','Descrente24','Descrente&amor300p','descronic');
-
-    if ($conn->connect_error) {
-        die('Connection failed'. $conn->connect_error);
-    }
-?>
